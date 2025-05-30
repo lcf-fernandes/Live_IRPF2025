f75b6e51c87106235e1f213c6c281ad76fc4e060
Leandro <leandrowebmaster@gmail.com>
2025-05-30 15:49:56 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -410,7 +410,7 @@ console.log({finalResult});
  const pass1 = { keyPath: [path2], value: [finalResult] };
  tools.functions.setVar({ args: '', pass: pass1 });
 
-tools.functions.goTo('scResultados');
+tools.goTo('scResultados');
 }, 
         (...args) => {
           // ---------- get Function from A_Project Scope
